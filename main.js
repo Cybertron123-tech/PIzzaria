590b63070396b086d618e57b2ed5ee4a19dee04b
Cybertron123-tech <71644024+Cybertron123-tech@users.noreply.github.com>
2020-12-23 14:21:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+menu_list_array - ["Chicken Tandoori Pizza",
+    "Veg Supreme Pizza" ,
+    "Paneer Tikka PIzza" ,
+    "Deluxe Veggie Pizza" ,
+    "Veg Extravaganza Pizza"
+];
+
+function getmenu()
